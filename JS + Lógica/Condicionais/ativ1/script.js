aa49a971263c7e7ed0a94526bdbaffce919b69ce
Endriw Schiavenato <endriwm.schiavenato@rede.ulbra.br>
2022-08-04 22:47:57 -0300

    Correção das pastas

--- JS + Lógica/Condicionais/ativ1/script.js
+++
@@ -1,6 +0,0 @@
-let spaceship = "Golias";
-let velocity = 80;
-
-console.log(velocity == 90) // false pq 80 n é igual 90
-console.log(spaceship == golias) // true pq a spaceship é Golias
-console.log(velocity === "80") // false pq o tipo numério '80' é diferente do texto '80'
--- JS + Lógica/Loops/do-while/script.js
+++
@@ -1,7 +0,0 @@
-let velocity = 110
-let acceleration = 5
-
-do {
-  console.log("Acelerando: Estamos a " + velocity + "km/s")
-  velocity += acceleration
-} while (velocity <= 100)
