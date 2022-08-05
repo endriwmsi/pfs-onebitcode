2847bd60df1dc9852df3630d314d03fac5d447c8
Endriw Schiavenato <endriwm.schiavenato@rede.ulbra.br>
2022-08-05 18:47:43 -0300

    Conclusão da Manipulação de DOM de JS + Lógica

---
+++ JS + Lógica/8 - Manipulando o DOM/Selecionando elementos/index.js
@@ -0,0 +1,4 @@
+function show() {
+  let elements = document.getElementsByTagName("div#phones iput[name='phone']")
+  console.log(elements)
+}
---
+++ JS + Lógica/8 - Manipulando o DOM/atividade1/index.js
@@ -0,0 +1,19 @@
+function clickMe() {
+  console.log("Fui clicado")
+}
+
+function focusMe() {
+  console.log("Fui focado")
+}
+
+function blurMe() {
+  console.log("Fui desfocado")
+}
+
+function changeMe() {
+  console.log("Fui mudado")
+}
+
+function overMe() {
+  console.log("Fui passado")
+}
